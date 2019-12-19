9aabca239be2997b6ce566e86cf1425f770fd8b6
Jiang, Lingbo (Data61, North Ryde) <Lingbo.Jiang@csiro.au>
2019-12-20 09:50:16 +1100

    AUS-3316 Setup the zoom level for geocoder search.

--- project/src/app/portal-core-ui/service/openlayermap/ol-map-object.ts
+++ project/src/app/portal-core-ui/service/openlayermap/ol-map-object.ts
@@ -105,10 +105,19 @@ export class OlMapObject {
       autoComplete: true,
       keepOpen: true
     });
-
+    let geocoderSource = GC.getSource();
+    const me = this;
+    GC.on('addresschosen', function (evt) {
+      let coord = evt.coordinate;
+      if (coord) {
+        geocoderSource.clear();
+        geocoderSource.addFeature(evt.feature); // add only the last one
+        me.map.getView().setCenter(coord);
+        me.map.getView().setZoom(9);
+      }
+    });
     this.map.addControl(GC);
     // Call a list of functions when the map is clicked on
-    const me = this;
     this.map.on('click', function(evt) {
       if (me.ignoreMapClick) {
         return;
