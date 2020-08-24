0ee9b86f9c4515cc99589833b4d3870fc30e1497
Vincent Fazio <vjf@users.noreply.github.com>
2020-08-24 15:07:59 +1000

    AUS-3425 Add build timestamp to portal UI

---
+++ docker/make_timestamp.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env ash
+echo "<div>"
+echo -n "<span>Auscope Portal Dev Build: "
+date +"%F %H:%M %Z"
+echo "</span><br>"
+echo '<a role="button" class="nav" href="#" (click)="OpenDisclaimerModal()">Disclaimer</a>'
+echo "</div>"
