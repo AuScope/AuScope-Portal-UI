50c754a916ebb4a831db5556854a06a284f85e67
woo392 <stuart.woodman@csiro.au>
2021-07-05 13:40:24 +1000

    Shift most of the sidebar menu items to a new information menu at the top right.

---
+++ project/src/app/toppanel/help-menu/help-menu.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HelpMenuComponent } from './help-menu.component';
+
+describe('HelpMenuComponent', () => {
+  let component: HelpMenuComponent;
+  let fixture: ComponentFixture<HelpMenuComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ HelpMenuComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HelpMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
