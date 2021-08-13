9c99afd5a0d36a9f7ac25621c77c3717aad3d535
woo392 <stuart.woodman@csiro.au>
2021-08-13 14:37:17 +1000

    IRIS layers can now be clicked.

--- project/src/app/cesium-map/custom-querier-handler/iris-querier-handler.service.ts
+++ project/src/app/cesium-map/custom-querier-handler/iris-querier-handler.service.ts
@@ -1,17 +1,21 @@
+import { LayerModel } from "@auscope/portal-core-ui";
+import { KmlFeatureData } from "cesium";
 
 
 export class IrisQuerierHandler {
 
-  constructor(private feature: any) {}
+  constructor(private layer: LayerModel, private entity: KmlFeatureData) {}
 
   public getHTML(): string {
-    let html = '<div class="row"><div class="col-md-3">Station</div><div class="col-md-9">' + this.feature.values_.name + '</div></div><hr>';
-    html += '<div class="row"><div class="col-md-3">Description</div><div class="col-md-9">' + this.feature.layer.description + '</div></div><hr>';
-    html += '<div class="row"><div class="col-md-3">Parser</div><div class="col-md-9">' + this.feature.values_.description + '</div></div><hr>';
-    html += '<div class="row"><div class="col-md-3">Layer</div><div class="col-md-9">' + this.feature.layer.group + '</div></div><hr>';
+    const extendedData = this.entity['kml']['extendedData'];
+    let html = '<div class="row"><div class="col-md-3">Station</div><div class="col-md-9">' + this.entity['name'] + '</div></div><hr>';
+    html += '<div class="row"><div class="col-md-3">Code</div><div class="col-md-9">' + extendedData['Code']['value'] + '</div></div><hr>';
+    html += '<div class="row"><div class="col-md-3">Country</div><div class="col-md-9">' + extendedData['Country']['value'] + '</div></div><hr>';
+    html += '<div class="row"><div class="col-md-3">Parser</div><div class="col-md-9">' + this.layer.description + '</div></div><hr>';
+    html += '<div class="row"><div class="col-md-3">Layer</div><div class="col-md-9">' + this.layer.group + '</div></div><hr>';
 
     html += '<div class="row"><div class="col-md-3">Record Info</div><div class="col-md-9">';
-    for (const cswRecord of this.feature.layer.cswRecords) {
+    for (const cswRecord of this.layer.cswRecords) {
       html += '<div class="row"><div class="col-md-3">Contact Org</div><div class="col-md-9">' + cswRecord.contactOrg + '</div></div><hr>';
       html += '<div class="row"><div class="col-md-3">Resource Description</div><div class="col-md-9">' + cswRecord.description + '</div></div><hr>';
       html += '<div class="row"><div class="col-md-3">Provider</div><div class="col-md-9">' + cswRecord.resourceProvider + '</div></div><hr>';
@@ -19,12 +23,7 @@ export class IrisQuerierHandler {
       html += '<div class="row"><div class="col-md-3">Source</div><div class="col-md-9"><a href="' + cswRecord.recordInfoUrl + '">Full Metadata and download</a></div></div><hr>';
     }
     html += '</div></div>';
-
     return html;
   }
-
-   public getKey(): string {
-    return this.feature.values_.name;
-  }
-
+  
 }
