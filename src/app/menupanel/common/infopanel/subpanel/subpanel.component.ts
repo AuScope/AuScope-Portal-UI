f23f4549dcf506bd802581a943536e514e2be99c
stuart.woodman <stuart.woodman@csiro.au>
2022-11-15 15:24:04 +1100

    Fix WMS URLs having a repeated service parameter and WFS URLs having a WMS service parameter.

--- src/app/menupanel/common/infopanel/subpanel/subpanel.component.ts
+++ src/app/menupanel/common/infopanel/subpanel/subpanel.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { CSWRecordModel } from '@auscope/portal-core-ui';
+import { CSWRecordModel, OnlineResourceModel } from '@auscope/portal-core-ui';
 import { LayerModel } from '@auscope/portal-core-ui';
 import { LegendService } from '@auscope/portal-core-ui';
 import { UtilitiesService } from '@auscope/portal-core-ui';
-
 
 
 @Component({
@@ -37,23 +36,48 @@ export class InfoPanelSubComponent implements OnChanges {
         }
     }
 
-
     /**
      * Remove unwanted and empty strings from metadata constraints fields
      * @param constraints string array of contraints
      * @return string constraints in string format
      */
     public cleanConstraints(constraints: string[]) {
-
         let outStr = "";
         for (const conStr of constraints) {
-            if (conStr.indexOf("no conditions apply") < 0 && 
-                conStr.indexOf("#MD_RestrictionCode") < 0 && conStr.trim() != "") {
+            if (conStr.indexOf("no conditions apply") < 0 &&
+                conStr.indexOf("#MD_RestrictionCode") < 0 && conStr.trim() !== "") {
                 outStr += conStr.trim() + ", ";
             }
         }
         // Remove trailing comma
         return outStr.replace(/, $/, "");
+    }
+
+    /**
+     * Create a WMS/WFS GetCapabilities URL from the provided OnlineResource
+     *
+     * @param onlineResource the OnlineResourceModel
+     * @returns a WMS or WFS GetCapabilities URL as a string
+     */
+    public onlineResourceGetCapabilitiesUrl(onlineResource: OnlineResourceModel): string {
+        // Determine base path, append mandatory service and request parameters
+        const paramIndex = onlineResource.url.indexOf('?');
+        let path = paramIndex !== -1 ? onlineResource.url.substr(0, onlineResource.url.indexOf('?')) : onlineResource.url;
+        path += '?service=' + onlineResource.type + '&request=GetCapabilities';
+        // Apend any other non-service or request parameters to path
+        if (paramIndex !== -1 && onlineResource.url.length > paramIndex + 1) {
+            const paramString = onlineResource.url.substr(paramIndex + 1, onlineResource.url.length);
+            const paramArray = paramString.split('&');
+            for (const keyValueString of paramArray) {
+                const keyValue = keyValueString.split('=');
+                if (keyValue.length === 2) {
+                    if (keyValue[0].toLowerCase() !== 'service' && keyValue[0].toLowerCase() !== 'request') {
+                        path += keyValue[0] + '=' + keyValue[1];
+                    }
+                }
+            }
+        }
+        return path;
     }
 
     /**
