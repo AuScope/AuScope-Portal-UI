1ba44601aa03286eab80328b64e4f594a40dfc73
Sam Bradley <sam.bradley@csiro.au>
2023-03-08 16:47:35 +0800

    Fix obvious linting errors and change default error to warnings

--- src/app/menupanel/portal-details-panel/portal-details-panel.component.ts
+++ src/app/menupanel/portal-details-panel/portal-details-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { DisclaimerModalComponent } from '../../modalwindow/disclaimer/disclaimer.modal.component';
 
@@ -7,12 +7,9 @@ import { DisclaimerModalComponent } from '../../modalwindow/disclaimer/disclaime
   templateUrl: './portal-details-panel.component.html',
   styleUrls: ['./portal-details-panel.component.css']
 })
-export class PortalDetailsPanelComponent implements OnInit {
+export class PortalDetailsPanelComponent {
 
   constructor(private modalService: BsModalService) { }
-
-  ngOnInit() {
-  }
 
   OpenDisclaimerModal(): void {
     this.modalService.show(DisclaimerModalComponent);
