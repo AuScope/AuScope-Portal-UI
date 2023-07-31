dc66c57b2954edc36d102a2b5506a9c7750f98e0
stuart.woodman <stuart.woodman@csiro.au>
2023-07-31 12:06:59 +1000

    Stop disclaimer window from immediately closing.

--- src/app/modalwindow/disclaimer/disclaimer.modal.component.ts
+++ src/app/modalwindow/disclaimer/disclaimer.modal.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-disclaimer-modal-window',
   templateUrl: './disclaimer.modal.component.html',
-  styleUrls: ['./disclaimer.modal.component.css']
+  styleUrls: ['./disclaimer.modal.component.scss']
 })
 export class DisclaimerModalComponent {
 
